c52b71119d97b712347ce413beae570013562072
DavidCervantes17 <41938035+DavidCervantes17@users.noreply.github.com>
2021-04-15 19:38:50 -0500

    Muestra respuestas

--- app/src/main/java/com/virtuallabs/makequiz/MainActivity.java
+++ app/src/main/java/com/virtuallabs/makequiz/MainActivity.java
@@ -8,13 +8,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.virtuallabs.makequiz.data.Item;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,14 +29,19 @@ public class MainActivity extends AppCompatActivity {
     String nombreCuestionario = "cuestionarios";
     EditText pregunta, respuesta;
     Button enviar;
+    TextView tv;
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    List<Item> items = new ArrayList<>();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv=(TextView)findViewById(R.id.tvSend);
+
 
         pregunta = findViewById(R.id.etPregunta);
         respuesta = findViewById(R.id.etRespuesta);
@@ -47,6 +59,11 @@ public class MainActivity extends AppCompatActivity {
                 item.put("pregunta", pregunta.getText().toString());
                 item.put("respuesta", respuesta.getText().toString());
 
+                Item miCuestionario = new Item();
+                miCuestionario.setPregunta(pregunta.getText().toString());
+                miCuestionario.setRespuesta(respuesta.getText().toString());
+                items.add(miCuestionario);
+
                 // Add a new document with a generated ID
                 db.collection("preguntas")
                         .add(item)
@@ -54,6 +71,8 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onSuccess(DocumentReference documentReference) {
                                 Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                                Toast.makeText(MainActivity.this,"Respuesta guardada",Toast.LENGTH_LONG).show();
+
                             }
                         })
                         .addOnFailureListener(new OnFailureListener() {
@@ -62,6 +81,19 @@ public class MainActivity extends AppCompatActivity {
                                 Log.w("TAG", "Error adding document", e);
                             }
                         });
+                StringBuilder sb = new StringBuilder();
+
+                for (Item s : items)
+                {
+                    sb.append(s);
+                }
+                Log.d("String",sb.toString());
+                tv.setText(sb.toString());
+
+
+                pregunta.setText("");
+                respuesta.setText("");
+
 
             }
         });
---
+++ app/src/main/java/com/virtuallabs/makequiz/data/Item.java
@@ -0,0 +1,28 @@
+package com.virtuallabs.makequiz.data;
+
+public class Item {
+    public String getPregunta() {
+        return pregunta;
+    }
+
+    public void setPregunta(String pregunta) {
+        this.pregunta = pregunta;
+    }
+
+    public String getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
+
+    String pregunta;
+    String respuesta;
+
+    @Override
+    public String toString() {
+        return "\n"+pregunta+": "+respuesta;
+    }
+
+}
